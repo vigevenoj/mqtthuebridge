011c9d5c748fe04581a75f99f4fdca66eed00429
Jacob Vigeveno <vigevenoj@gmail.com>
2016-12-08 11:39:09 -0800

    Initial commit

---
+++ src/main/java/com/sharkbaitextraordinaire/mqtthuebridge/MqtthuebridgeApplication.java
@@ -0,0 +1,12 @@
+package com.sharkbaitextraordinaire.mqtthuebridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqtthuebridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqtthuebridgeApplication.class, args);
+	}
+}
